da747ee2219544aebcd88030f07cd8c726cec1ab
Mata Prasad Chauhan <mataprasad045@gmail.com>
2019-01-28 19:58:45 +0530

    accept post data on view end points

--- app/resources/home.py
+++ app/resources/home.py
@@ -1,9 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restplus import Namespace, Resource
 
 api = Namespace('home', description='Status related stuff')
 
+parser = api.parser()
+parser.add_argument('resource_name', type=str, help='Resource name', location='form')
+parser.add_argument('attribute_name', type=str, help='Attribute name', location='form')
+parser.add_argument('resource_value', type=str, help='Attribute value', location='form')
+parser.add_argument('config_name', type=str, help='Config name', location='form')
+
 @api.route("/")
 class Home(Resource):
-    def get(self):
-        return "home", 200
+    @api.doc(parser=parser)
+    def post(self):
+        args = parser.parse_args()
+        data = {'msg':"home "+ args['resource_name']}
+        return data, 200
--- app/views/home.py
+++ app/views/home.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.util.test import test
 from core.util.db_util import __test_db_connection
 
 bp = Blueprint('home-view', __name__)
 
-@bp.route("/")
+@bp.route("/",methods=['GET','POST'])
 def home():
-    __test_db_connection()
-    return render_template("home.html",data=test())
+    #__test_db_connection()
+    data = []
+    if request.method == 'POST':
+        data = [ [request.form['resource_name']] ]
+    return render_template("home.html",data=data)
