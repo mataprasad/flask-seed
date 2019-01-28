98787ce2df6e3311ff45d5d264beb28179d67816
Mata Prasad Chauhan <mataprasad045@gmail.com>
2019-01-29 00:11:06 +0530

    url for api

--- core/Api.py
+++ core/Api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restplus import Api
 
-bp = Blueprint('api', __name__, url_prefix='/v1/api')
+bp = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(bp,
     title='API',
     description='API'
